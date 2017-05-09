f73aeb056889ff069720947b611934c14707adec
zyhndesign <zyhndesign@zyhnmatoMacBook-Air.local>
2017-05-09 15:38:45 +0800

    去除乱码

--- src/main/java/com/cidic/design/realm/UserRealm.java
+++ src/main/java/com/cidic/design/realm/UserRealm.java
@@ -52,7 +52,7 @@ public class UserRealm extends AuthorizingRealm{
          */
         //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以自定义实�??
         SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
-                user.get().getEmail(), //用户�??
+                user.get().getEmail(), //用户
                 user.get().getPassword(), //密码
                 ByteSource.Util.bytes(user.get().getCredentialsSalt()),
                 getName()  //realm name

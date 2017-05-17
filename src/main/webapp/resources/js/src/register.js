7904fd615ef5d375d36a15b517ef51a3d12e8f4c
csboyty <csboyty@gmail.com>
2017-05-17 09:17:01 +0800

    up

--- src/main/webapp/resources/js/src/register.js
+++ src/main/webapp/resources/js/src/register.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
                 rangelength:[6,20]
             },
             confirmPwd:{
-                equalTo:"#newPwd"
+                equalTo:"#password"
             }
         },
         messages:{

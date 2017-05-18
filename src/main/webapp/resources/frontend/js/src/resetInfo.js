9392e5c822d2272161c73987c273315cf2cb29a1
csboyty <csboyty@gmail.com>
2017-05-18 10:15:42 +0800

    up

--- src/main/webapp/resources/frontend/js/src/resetInfo.js
+++ src/main/webapp/resources/frontend/js/src/resetInfo.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
             }
         },
         submitHandler:function(form) {
-            zyFormHandler.submitForm(form);
+            zyFormHandler.submitFormWithJSON(form);
         }
     });
 });

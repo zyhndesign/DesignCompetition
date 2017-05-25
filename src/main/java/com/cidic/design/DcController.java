3d4135ad9b3afe30ab605f034debdcc257a3aac5
codeliling <maximuslee@126.com>
2017-05-25 14:23:12 +0800

    fix bugs

--- src/main/java/com/cidic/design/DcController.java
+++ src/main/java/com/cidic/design/DcController.java
@@ -1,5 +1,6 @@
 package com.cidic.design;
 
+import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,6 +36,18 @@ public abstract class DcController {
 		resultModel.setMessage("无权限进行操作");
 		resultModel.setSuccess(false);
 		return resultModel;
+	}
+	
+	
+	@ExceptionHandler(AuthorizationException.class)
+	public ModelAndView handleAuthorizationException(AuthorizationException ex) {
+		ModelAndView modelView = new ModelAndView();
+		resultModel = new ResultModel();
+		resultModel.setResultCode(911);
+		resultModel.setMessage("没有进行身份认证");
+		resultModel.setSuccess(false);
+		modelView.setViewName("error");
+        return modelView;
 	}
 	
 	@ExceptionHandler(DcRedirectException.class)
